745b3a48d6d011ac86203e72cc80df09d30316ab
Michael Peters <michaelpeterswa@icloud.com>
2021-02-19 16:42:07 -0800

    feat: ready for new model

--- src/server.py
+++ src/server.py
@@ -1,15 +1,26 @@
 from flask import Flask, request, jsonify
 from tensorflow import keras
+import numpy as np
 import math
 
 model = keras.models.load_model('../kulo_model')
 app = Flask(__name__)
 
-def predict(latitude, longitude):
-    lat_amt = 100
-    long_amt = 200
+# https://www.raspberrypi.org/forums/viewtopic.php?t=149371#p982264
+def valmap(value, istart, istop, ostart, ostop):
+  return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
 
-    return model.predict([(latitude / lat_amt, longitude / long_amt)])
+def predict(epochx, latx, longx):
+    tscl = (5040000.0, 1612339200.0, 0, 1)
+    ltscl = (45.556224, 49.00112, 0, 1)
+    lnscl = (-124.716371, -116.94347, 0, 1)
+
+    epochx = valmap(epochx, tscl[0], tscl[1], tscl[2], tscl[3])
+    latx = valmap(latx, ltscl[0], ltscl[1], ltscl[2], ltscl[3])
+    longx = valmap(longx, lnscl[0], lnscl[1], lnscl[2], lnscl[3])
+
+    inp = np.array([(epochx, latx, longx)])
+    return model.predict(inp)
 
 def acres_to_circle_radius_in_miles(acres):
     sqft = acres * 43560
@@ -23,12 +34,16 @@ def main_route():
 @app.route('/api/predict')
 def return_prediction():
     max_acreage = 255900
+    epoch_req = float(request.args.get('epoch'))
     lat_req = float(request.args.get('lat'))
     long_req = float(request.args.get('long'))
 
-    prediction = predict(lat_req, long_req)
-    acres = prediction[0][0] * max_acreage
+    ascl = (0.0, 250280.45, 0, 1)
+
+    prediction = predict(epoch_req, lat_req, long_req)
+    acres = valmap(prediction[0][0], ascl[2], ascl[3], ascl[0], ascl[1])
     result = {
+        "epoch": epoch_req,
         "lat": lat_req,
         "long": long_req,
         "acres": acres,
